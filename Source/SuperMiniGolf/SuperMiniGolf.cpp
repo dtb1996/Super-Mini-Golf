10f8a16c29f2d3559ddbb99b565fe46a34e85a13
Dillon Bellefeuille <dtb1996@yahoo.com>
2025-01-07 01:03:13 -0500

    Added project files

---
+++ Source/SuperMiniGolf/SuperMiniGolf.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SuperMiniGolf.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SuperMiniGolf, "SuperMiniGolf" );

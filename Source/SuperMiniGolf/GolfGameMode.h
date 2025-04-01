b04cb1a42c1dc4dc83ba7a22bf994902c104ff11
Dillon Bellefeuille <dtb1996@yahoo.com>
2025-03-31 21:27:55 -0400

    Added GolfGameMode class and reparented BP_GolfGameMode

---
+++ Source/SuperMiniGolf/GolfGameMode.h
@@ -0,0 +1,14 @@
+// Copyright 2025, Bit Gaming Studio. All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GolfGameMode.generated.h"
+
+UCLASS(Abstract)
+class SUPERMINIGOLF_API AAGolfGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};

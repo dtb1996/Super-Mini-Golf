66d84dc1efe57e5a6143c6c46e1394d12e3dbb7a
Dillon Bellefeuille <dtb1996@yahoo.com>
2025-04-04 17:17:41 -0400

    Moved input device check logic into GolfGameInstance c++ class

--- Source/SuperMiniGolf/GolfGameInstance.cpp
+++ Source/SuperMiniGolf/GolfGameInstance.cpp
@@ -1,6 +1,49 @@
 // Copyright 2025, Bit Gaming Studio. All Rights Reserved
 
 #include "GolfGameInstance.h"
+#include "GameFramework/InputDeviceSubsystem.h"
+#include "Engine/Engine.h"
+
+void UGolfGameInstance::Init()
+{
+	Super::Init();
+
+	UInputDeviceSubsystem* InputDeviceSubsystem = GEngine->GetEngineSubsystem<UInputDeviceSubsystem>();
+	if (InputDeviceSubsystem)
+	{
+		InputDeviceSubsystem->OnInputHardwareDeviceChanged.AddDynamic(this, &UGolfGameInstance::OnInputTypeChanged);
+	}
+}
+
+void UGolfGameInstance::Shutdown()
+{
+	UInputDeviceSubsystem* InputDeviceSubsystem = GEngine->GetEngineSubsystem<UInputDeviceSubsystem>();
+	if (InputDeviceSubsystem)
+	{
+		InputDeviceSubsystem->OnInputHardwareDeviceChanged.RemoveDynamic(this, &UGolfGameInstance::OnInputTypeChanged);
+	}
+
+	Super::Shutdown();
+}
+
+const bool UGolfGameInstance::GetIsGamepadInput(const FPlatformUserId UserId) const
+{
+	UInputDeviceSubsystem* InputDeviceSubsystem = GEngine->GetEngineSubsystem<UInputDeviceSubsystem>();
+	if (!InputDeviceSubsystem)
+	{
+		return false;
+	}
+
+	FHardwareDeviceIdentifier HardwareDevice = InputDeviceSubsystem->GetMostRecentlyUsedHardwareDevice(UserId);
+	if (!HardwareDevice.IsValid())
+	{
+		return false;
+	}
+
+	FString DeviceTypeString = UEnum::GetValueAsString(HardwareDevice.PrimaryDeviceType);
+
+	return DeviceTypeString.Contains(TEXT("Gamepad"));
+}
 
 void UGolfGameInstance::SetPlayerStats(const int PlayerId, const FPlayerStats NewPlayerStats)
 {
@@ -38,4 +81,11 @@ void UGolfGameInstance::SetPlayerScore(const int PlayerId, const int NewPlayerSc
 	FoundStats->PlayerScore = NewPlayerScore;
 
 	PlayerStats.Add(PlayerId, *FoundStats);
+}
+
+void UGolfGameInstance::OnInputTypeChanged(const FPlatformUserId UserId, const FInputDeviceId DeviceId)
+{
+	bIsGamepadInput = GetIsGamepadInput(UserId);
+
+	OnInputHardwareChanged.Broadcast(bIsGamepadInput);
 }
--- Source/SuperMiniGolf/GolfGameInstance.h
+++ Source/SuperMiniGolf/GolfGameInstance.h
@@ -7,12 +7,24 @@
 #include "PlayerStats.h"
 #include "GolfGameInstance.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnInputHardwareChanged, bool, bIsGamepadInput);
+
 UCLASS(Abstract)
 class SUPERMINIGOLF_API UGolfGameInstance : public UGameInstance
 {
 	GENERATED_BODY()
 	
 public:
+	virtual void Init() override;
+
+	virtual void Shutdown() override;
+
+	UPROPERTY(BlueprintAssignable, Category = "Player Input")
+	FOnInputHardwareChanged OnInputHardwareChanged;
+
+	UFUNCTION(BlueprintCallable, Category = "Player Input")
+	const bool GetIsGamepadInput(const FPlatformUserId UserId) const;
+
 	UFUNCTION(BlueprintCallable, Category = "Player Info")
 	void SetPlayerStats(const int PlayerId, const FPlayerStats NewPlayerStats);
 
@@ -26,5 +38,12 @@ public:
 	void SetPlayerScore(const int PlayerId, const int NewPlayerScore);
 
 private:
+	UFUNCTION()
+	void OnInputTypeChanged(const FPlatformUserId UserId, const FInputDeviceId DeviceId);
+
+	FDelegateHandle InputDeviceChangedHandle;
+
+	bool bIsGamepadInput = false;
+
 	TMap<int, FPlayerStats> PlayerStats;
 };
